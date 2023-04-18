9617c171a6a699090af8f2940de54a83c362a3fc
Jorge Aguilar <anim85jorge@gmail.com>
2023-04-18 16:08:51 -0700

    setting up the Navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+import Navbar from "./scenes/Navbar"
 import { useState } from "react";
-import useMediaQuery from ".hooks/useMediaQuery"
+import useMediaQuery from "./hooks/useMediaQuery"
 
 
 function App() {
-  const [ selectedPage, setSelectedPage] = useState('home');
+  const [ selectedPage, setSelectedPage] = useState("home");
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
   
   
   return <div className="app bg-deep-blue" >
 <Navbar
-
-
+selectedPage={selectedPage}
+setSelectedPage={setSelectedPage}
 />
-
   </div>
 }
 
 export default App;
 
-/* 00:22:54 time Left off */
+
